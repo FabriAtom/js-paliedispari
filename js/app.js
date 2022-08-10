a01e3d3591ff29594588b891660ee8d70eaedc56
FabriAtom <fabrizioromano20@gmail.com>
2022-08-10 17:03:41 +0200

    /js compl

--- js/app.js
+++ js/app.js
@@ -1,14 +1,22 @@
 // Chiedere all’utente di inserire una parola
-
 function isPalindrome(parola) {
-    parola = parola.toLoweCase()
-    return parola === invertiParola(parola);
+    parola = parola.toLowerCase()
+    const parolaInvertita = invertiParola(parola)
+
+    return parola === parolaInvertita;
 }
+
 
 function invertiParola(testo) {
-    return testo.split('').reverse().join('');
+    const caratteriDiParola = testo.split('')
+    console.log(caratteriDiParola)
+    const caratteriInvertiti = caratteriDiParola.reverse()
+    console.log(caratteriInvertiti)
+    const parolaInvertita = caratteriInvertiti.join('')
+    console.log(parolaInvertita)
+
+    return parolaInvertita
 }
 
-const parola = prompt('inserisci una parola e ti dirò se è palindorma');
-
-console.log(isPalindrome(parola));
+const p = prompt('Inserisci una parola e ti dirò se è palindroma.')
+ console.log(isPalindrome(p)); //amoroma
